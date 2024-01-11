7ba5783f64b9cd13c05c56107cfac77123c52c90
Reis567 <matheusdosreis9@gmail.com>
2024-01-11 07:09:11 -0300

    called the method save of repository to save a user

--- src/main/java/com/reis/demo/park/api/service/UsuarioService.java
+++ src/main/java/com/reis/demo/park/api/service/UsuarioService.java
@@ -2,8 +2,10 @@ package com.reis.demo.park.api.service;
 
 import org.springframework.stereotype.Service;
 
+import com.reis.demo.park.api.entity.Usuario;
 import com.reis.demo.park.api.repository.UsuarioRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -12,5 +14,9 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     
+    @Transactional
+    public Usuario salvar (Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
 
 }
