831fc751c8004e3a052677f55fd359f9df3a0060
Reis567 <matheusdosreis9@gmail.com>
2024-01-12 06:42:24 -0300

    get te user from id method

--- src/main/java/com/reis/demo/park/api/web/controller/UsuarioController.java
+++ src/main/java/com/reis/demo/park/api/web/controller/UsuarioController.java
@@ -2,6 +2,8 @@ package com.reis.demo.park.api.web.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +27,11 @@ public class UsuarioController {
         Usuario user = usuarioService.salvar(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }  
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> GetById(@PathVariable Long id){
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
     
 }
