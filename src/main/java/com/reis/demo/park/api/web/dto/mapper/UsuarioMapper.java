7b483deb5dc6f94057bc1452984051e255b49631
Reis567 <matheusdosreis9@gmail.com>
2024-01-13 19:16:12 -0300

    method mapper to convert User model to DTO

--- src/main/java/com/reis/demo/park/api/web/dto/mapper/UsuarioMapper.java
+++ src/main/java/com/reis/demo/park/api/web/dto/mapper/UsuarioMapper.java
@@ -1,13 +1,28 @@
 package com.reis.demo.park.api.web.dto.mapper;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 
 import com.reis.demo.park.api.entity.Usuario;
 import com.reis.demo.park.api.web.dto.UsuarioCreateDTO;
+import com.reis.demo.park.api.web.dto.UsuarioResponseDTO;
 
 public class UsuarioMapper {
 
     public static Usuario toUsuario(UsuarioCreateDTO usuarioCreateDTO){
         return new ModelMapper().map(usuarioCreateDTO, Usuario.class);
     }
+
+    public static UsuarioResponseDTO toDTO(Usuario usuario){
+        String role = usuario.getRole().name().substring("ROLE_".length());
+        PropertyMap<Usuario,UsuarioResponseDTO> propertyMap= new PropertyMap<Usuario,UsuarioResponseDTO>() {
+            @Override
+            protected void configure(){
+                map().setRole(role);
+            }
+        };
+
+        ModelMapper modelMapper =  new ModelMapper();
+        modelMapper.addMappings(propertyMap);
+        return modelMapper.map(usuario, UsuarioResponseDTO.class);
 }
