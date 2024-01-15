a53366f248764abe3990034121f25910adcd9cfd
Reis567 <matheusdosreis9@gmail.com>
2024-01-15 18:08:27 -0300

    ererorMessage constrtuctor with result variable

--- src/main/java/com/reis/demo/park/api/web/exception/ErrorMessage.java
+++ src/main/java/com/reis/demo/park/api/web/exception/ErrorMessage.java
@@ -1,9 +1,17 @@
 package com.reis.demo.park.api.web.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 import jakarta.servlet.http.HttpServletRequest;
-
+import lombok.Getter;
+import lombok.ToString;
+ 
+@Getter @ToString
 public class ErrorMessage {
 
     private String path;
@@ -15,12 +23,31 @@ public class ErrorMessage {
     private String statusText;
 
     private String message;
- 
+
+    private Map<String , String> errors;
+
+    public ErrorMessage(){
+
+    }
     public ErrorMessage(HttpServletRequest request , HttpStatus status, String message){
         this.path = request.getRequestURI();
         this.method = request.getMethod();
         this.status = status.value();
         this.statusText = status.getReasonPhrase();
         this.message = message;
+    }
+    public ErrorMessage(HttpServletRequest request , HttpStatus status, String message, BindingResult result){
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+        addErrors(result);
+    }
+    private Void addErrors(BindingResult result){
+        this.errors = new HashMap<>();
+        for(FieldError fieldError : result.getFieldErrors()){
+            this.errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
     }
 }
