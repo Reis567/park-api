6811f8d9c70cb32fb1e983c9cd225f90bfb1ddd4
Reis567 <matheusdosreis9@gmail.com>
2024-01-15 17:46:40 -0300

    Error items

--- src/main/java/com/reis/demo/park/api/web/exception/ErrorMessage.java
+++ src/main/java/com/reis/demo/park/api/web/exception/ErrorMessage.java
@@ -1,5 +1,18 @@
 package com.reis.demo.park.api.web.exception;
 
 public class ErrorMessage {
-    
+
+    private String path;
+
+    private String method;
+
+    private int status;
+
+    private String statusText;
+
+    private String message;
+ 
+    public ErrorMessage(){
+        
+    }
 }
