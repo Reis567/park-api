2808ab28d0ab82ec11884c5f312a4b41838708dc
Reis567 <matheusdosreis9@gmail.com>
2024-01-15 07:43:04 -0300

    email data validation

--- src/main/java/com/reis/demo/park/api/web/dto/UsuarioCreateDTO.java
+++ src/main/java/com/reis/demo/park/api/web/dto/UsuarioCreateDTO.java
@@ -1,11 +1,14 @@
 package com.reis.demo.park.api.web.dto;
 
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class UsuarioCreateDTO {
 
+    @Email(message = "Formato de emai. Inválido")
+    @NotBlank
     private String username;
     private String password;
 }
