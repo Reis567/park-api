4c3e21dc839b9bda7aa430e65edd60f95743b76a
Reis567 <matheusdosreis9@gmail.com>
2024-01-15 07:53:19 -0300

    password dto validation

--- src/main/java/com/reis/demo/park/api/web/dto/UsuarioSenhaDTO.java
+++ src/main/java/com/reis/demo/park/api/web/dto/UsuarioSenhaDTO.java
@@ -1,12 +1,22 @@
 package com.reis.demo.park.api.web.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class UsuarioSenhaDTO {
+
+    @NotBlank
+    @Size(min = 6)
     private String senhaAtual;
+    
+    @NotBlank
+    @Size(min = 6)
     private String novaSenha;
+
+    @NotBlank
+    @Size(min = 6)
     private String confirmaSenha;
 }
     
