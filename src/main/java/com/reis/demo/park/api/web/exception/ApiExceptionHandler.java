ccb00c8d8a99c0c3c28ebacb051918d650140f69
Reis567 <matheusdosreis9@gmail.com>
2024-01-16 07:26:48 -0300

    return the status , the content type , body and message of error

--- src/main/java/com/reis/demo/park/api/web/exception/ApiExceptionHandler.java
+++ src/main/java/com/reis/demo/park/api/web/exception/ApiExceptionHandler.java
@@ -1,16 +1,22 @@
 package com.reis.demo.park.api.web.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(){
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException exception , HttpServletRequest request , BindingResult result){
 
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).contentType(MediaType.APPLICATION_JSON).body(new ErrorMessage(request,HttpStatus.UNPROCESSABLE_ENTITY, "Campos inválidos !", result ));
     }
     
 }
