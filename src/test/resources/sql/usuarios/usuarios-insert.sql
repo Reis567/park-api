0fc6567a7bf4a6b4371db05c5d2e84f739edbe6b
Reis567 <matheusdosreis9@gmail.com>
2024-01-22 17:28:37 -0300

    initial setup of H2 Database to run tests

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+insert into USUARIOS( id, username,password, role) values (100,'ana@gmail.com','123456','ROLE_ADMIN');
+insert into USUARIOS( id, username,password, role) values (101,'JOAO@gmail.com','123456','ROLE_CLIENTE');
+insert into USUARIOS( id, username,password, role) values (102,'davi@gmail.com','123456','ROLE_CLIENTE');
