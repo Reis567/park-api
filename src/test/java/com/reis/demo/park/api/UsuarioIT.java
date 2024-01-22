481f797cb61af58a264118f938183e9c859514a4
Reis567 <matheusdosreis9@gmail.com>
2024-01-22 17:59:21 -0300

    Test 1 : Create user test

--- src/test/java/com/reis/demo/park/api/UsuarioIT.java
+++ src/test/java/com/reis/demo/park/api/UsuarioIT.java
@@ -1,9 +1,14 @@
 package com.reis.demo.park.api;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.reis.demo.park.api.web.dto.UsuarioCreateDTO;
+import com.reis.demo.park.api.web.dto.UsuarioResponseDTO;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "/sql/usuarios/usuarios-insert.sql" ,executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
@@ -12,4 +17,22 @@ public class UsuarioIT {
     
     @Autowired
     WebTestClient testClient;
+
+    @Test
+    public void createUsuario_ComUsernameEPasswordValidos_RetornarUsuarioCriadoComStatus201(){
+        UsuarioResponseDTO responseBody = testClient
+            .post()
+            .uri("/api/v1/usuarios/registro")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(new UsuarioCreateDTO("toby@email.com","123456"))
+            .exchange()
+            .expectStatus().isCreated()
+            .expectBody(UsuarioResponseDTO.class)
+            .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("toby@email.com");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENTE");
+    }
 }
