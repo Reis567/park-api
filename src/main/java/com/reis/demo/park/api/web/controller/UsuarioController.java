30f4fd7894b5937e74abe654d09b5a69f1f4769f
Reis567 <matheusdosreis9@gmail.com>
2024-01-22 07:13:34 -0300

    Documentation of User Creation

--- src/main/java/com/reis/demo/park/api/web/controller/UsuarioController.java
+++ src/main/java/com/reis/demo/park/api/web/controller/UsuarioController.java
@@ -18,10 +18,17 @@ import com.reis.demo.park.api.web.dto.UsuarioCreateDTO;
 import com.reis.demo.park.api.web.dto.UsuarioResponseDTO;
 import com.reis.demo.park.api.web.dto.UsuarioSenhaDTO;
 import com.reis.demo.park.api.web.dto.mapper.UsuarioMapper;
+import com.reis.demo.park.api.web.exception.ErrorMessage;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name="Usuários", description = "Contém todas as operações relativas ao cadastro edição e leitura de usuários . ")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/usuarios")
@@ -30,6 +37,20 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
 
+    @Operation(summary = "Criar um novo usuário",
+                description = "Recurso para criar um novo usuário",
+                responses = {
+                    @ApiResponse(responseCode = "201",description = "Recurso criado com sucesso "
+                    , content = @Content(mediaType = "application/json",schema = @Schema(implementation = UsuarioResponseDTO.class))),
+
+                    @ApiResponse(responseCode = "409", description = "Usuário email já cadastrado no sistema",
+                    content = @Content(mediaType = "application/json",schema = @Schema(implementation = ErrorMessage.class))),
+
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por dados de entrada inválidos",
+                    content = @Content(mediaType = "application/json",schema = @Schema(implementation = ErrorMessage.class))),
+                    
+                }
+        )
     @PostMapping("/registro")
     public ResponseEntity<UsuarioResponseDTO> create (@Valid @RequestBody UsuarioCreateDTO usuarioCreateDTO){
         Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(usuarioCreateDTO));
