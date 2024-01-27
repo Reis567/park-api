a3228d37200335b0b851a3132d1afb36f14eaf7d
Reis567 <matheusdosreis9@gmail.com>
2024-01-27 19:42:54 -0300

    Initial Auditing setup

---
+++ src/main/java/com/reis/demo/park/api/config/SpringJpaAuditingConfig.java
@@ -0,0 +1,10 @@
+package com.reis.demo.park.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class SpringJpaAuditingConfig {
+    
+}
