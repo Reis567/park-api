ab67a9d65d357d6bcdc945df1ea444c445cef1b8
Reis567 <matheusdosreis9@gmail.com>
2024-01-27 19:46:20 -0300

    Auditor aware needs a method called get current auditor

--- src/main/java/com/reis/demo/park/api/config/SpringJpaAuditingConfig.java
+++ src/main/java/com/reis/demo/park/api/config/SpringJpaAuditingConfig.java
@@ -1,5 +1,7 @@
 package com.reis.demo.park.api.config;
 
+import java.util.Optional;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,5 +9,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @Configuration
 public class SpringJpaAuditingConfig implements AuditorAware<String>{
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.empty();
+    }
     
 }
