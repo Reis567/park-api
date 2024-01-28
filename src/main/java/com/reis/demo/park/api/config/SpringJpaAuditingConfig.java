165aa85a73c43727e9e93f4aba2f2619b1e2a243
Reis567 <matheusdosreis9@gmail.com>
2024-01-28 10:50:21 -0300

    Get the name of the current user

--- src/main/java/com/reis/demo/park/api/config/SpringJpaAuditingConfig.java
+++ src/main/java/com/reis/demo/park/api/config/SpringJpaAuditingConfig.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @EnableJpaAuditing
 @Configuration
@@ -12,7 +14,11 @@ public class SpringJpaAuditingConfig implements AuditorAware<String>{
 
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.empty();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()){
+            return Optional.of(authentication.getName());
+        }
+        return null;
     }
     
 }
