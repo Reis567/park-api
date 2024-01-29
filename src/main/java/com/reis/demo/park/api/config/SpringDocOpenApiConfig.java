6a1a6ecd813b5eee3c77a0ff4109398824cbed48
Reis567 <matheusdosreis9@gmail.com>
2024-01-29 07:59:20 -0300

    Open api security scheme

--- src/main/java/com/reis/demo/park/api/config/SpringDocOpenApiConfig.java
+++ src/main/java/com/reis/demo/park/api/config/SpringDocOpenApiConfig.java
@@ -3,16 +3,20 @@ package com.reis.demo.park.api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SpringDocOpenApiConfig {
     @Bean
     public OpenAPI openAPI(){
         return new OpenAPI()
+            .components(new Components().addSecuritySchemes("security",securityScheme()))
             .info(
                 new Info()
                     .title("Rest API - Park API")
@@ -22,4 +26,14 @@ public class SpringDocOpenApiConfig {
                     .contact(new Contact().name("Matheus dos reis").email("matheusdosreisdev@gmail.com"))
             ); 
     }
+
+    private SecurityScheme securityScheme (){
+        return new SecurityScheme().description("Insira um Bearer token valido para prosseguir")
+        .type(SecurityScheme.Type.HTTP)
+        .in(SecurityScheme.In.HEADER)
+        .scheme("bearer")
+        .bearerFormat("JWT")
+        .name("security");
+    }
+
 }
