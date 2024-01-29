793d19685ed7475475af13588bbd31ca252e942f
Reis567 <matheusdosreis9@gmail.com>
2024-01-29 16:59:43 -0300

    On sql insert , of the test module , changed the pass to a bcrypt pass

--- src/test/resources/sql/usuarios/usuarios-insert.sql
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,3 +1,3 @@
-insert into USUARIOS( id, username,password, role) values (100,'ana@gmail.com','123456','ROLE_ADMIN');
-insert into USUARIOS( id, username,password, role) values (101,'JOAO@gmail.com','123456','ROLE_CLIENTE');
-insert into USUARIOS( id, username,password, role) values (102,'davi@gmail.com','123456','ROLE_CLIENTE');
+insert into USUARIOS( id, username,password, role) values (100,'ana@gmail.com','$2a$12$4G1.TGGdDn2BeBBjA/bXZ.xpAIcyjGqupKqYKLG55wxCZsKXgPuIu','ROLE_ADMIN');
+insert into USUARIOS( id, username,password, role) values (101,'JOAO@gmail.com','$2a$12$4G1.TGGdDn2BeBBjA/bXZ.xpAIcyjGqupKqYKLG55wxCZsKXgPuIu','ROLE_CLIENTE');
+insert into USUARIOS( id, username,password, role) values (102,'davi@gmail.com','$2a$12$4G1.TGGdDn2BeBBjA/bXZ.xpAIcyjGqupKqYKLG55wxCZsKXgPuIu','ROLE_CLIENTE');
