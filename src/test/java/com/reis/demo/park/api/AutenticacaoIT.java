073491ddbf5dc35633ddd9626f534193f303dbec
Reis567 <matheusdosreis9@gmail.com>
2024-01-31 18:30:59 -0300

    New test : Try to auth with invalid password

--- src/test/java/com/reis/demo/park/api/AutenticacaoIT.java
+++ src/test/java/com/reis/demo/park/api/AutenticacaoIT.java
@@ -77,5 +77,17 @@ public class AutenticacaoIT {
         org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
         org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
 
+        responseBody = testClient
+        .post()
+        .uri("api/v1/auth")
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(new UsuarioLoginDTO("ana@gmail.com","12345"))
+        .exchange()
+        .expectStatus().isEqualTo(422)
+        .expectBody(ErrorMessage.class)
+        .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
     }
 }
