6b6532d9448520a5b6d498daf9b4fe434029d537
Reis567 <matheusdosreis9@gmail.com>
2024-02-01 07:29:52 -0300

    Mapper : Method toDTO

--- src/main/java/com/reis/demo/park/api/web/dto/mapper/ClienteMapper.java
+++ src/main/java/com/reis/demo/park/api/web/dto/mapper/ClienteMapper.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 
 import com.reis.demo.park.api.entity.Cliente;
 import com.reis.demo.park.api.web.dto.ClienteCreateDTO;
+import com.reis.demo.park.api.web.dto.ClienteResponseDTO;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,8 @@ public class ClienteMapper {
     
     public static Cliente toCliente(ClienteCreateDTO clienteCreateDTO){
         return new ModelMapper().map(clienteCreateDTO, Cliente.class);
+    }
+    public static ClienteResponseDTO toDTO(Cliente cliente){
+        return new ModelMapper().map(cliente, ClienteResponseDTO.class);
     }
 }
