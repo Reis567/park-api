44d4e1fad1e53395dd0bf3ae1033897179d87af1
Reis567 <matheusdosreis9@gmail.com>
2024-02-01 07:07:07 -0300

    DTO of Create client

---
+++ src/main/java/com/reis/demo/park/api/web/dto/ClienteCreateDTO.java
@@ -0,0 +1,16 @@
+package com.reis.demo.park.api.web.dto;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class ClienteCreateDTO {
+    @NotBlank @Size(min = 3 ,max = 130)
+    private String nome;
+
+    @NotBlank @Size(min = 11 ,max = 11) @CPF
+    private String cpf;
+}
