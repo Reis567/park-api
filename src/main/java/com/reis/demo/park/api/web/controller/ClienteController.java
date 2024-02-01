7b74b778bd1a18770042cc13126db642491e2e2a
Reis567 <matheusdosreis9@gmail.com>
2024-02-01 08:08:25 -0300

    Initial setup of Client controller

---
+++ src/main/java/com/reis/demo/park/api/web/controller/ClienteController.java
@@ -0,0 +1,13 @@
+package com.reis.demo.park.api.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/clientes")
+public class ClienteController {
+    
+}
