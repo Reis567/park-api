503ac706e1b75c22b4a7544638201858fca2b7a6
Reis567 <matheusdosreis9@gmail.com>
2024-02-01 07:45:37 -0300

    Initial logic to save a Client and Cpf unique exception

---
+++ src/main/java/com/reis/demo/park/api/exception/CpfUniqueViolationException.java
@@ -0,0 +1,7 @@
+package com.reis.demo.park.api.exception;
+
+public class CpfUniqueViolationException extends RuntimeException{ 
+    public CpfUniqueViolationException( String message){
+        super(message)
+    }
+}
--- src/main/java/com/reis/demo/park/api/service/ClienteService.java
+++ src/main/java/com/reis/demo/park/api/service/ClienteService.java
@@ -1,7 +1,9 @@
 package com.reis.demo.park.api.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.reis.demo.park.api.entity.Cliente;
 import com.reis.demo.park.api.repository.ClienteRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -12,4 +14,13 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ClienteService {
     private final ClienteRepository clienteRepository;
+
+    @Transactional
+    public Cliente salvar (Cliente cliente){
+        try {
+            clienteRepository.save(cliente);
+        } catch (org.springframework.dao.DataIntegrityViolationException ex) {
+            throw new CpfUniqueViolationException("");
+        }
+    }
 }
