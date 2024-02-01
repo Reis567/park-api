77f3e661aeb2d30eac85a8722fa86990d6d0cb6b
Reis567 <matheusdosreis9@gmail.com>
2024-02-01 08:24:33 -0300

    return the response entity with the response DTO

--- src/main/java/com/reis/demo/park/api/web/controller/ClienteController.java
+++ src/main/java/com/reis/demo/park/api/web/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package com.reis.demo.park.api.web.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +30,7 @@ public class ClienteController {
         cliente.setUsuario(usuarioService.buscarPorId(userDetails.getId()));
 
         clienteService.salvar(cliente);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(ClienteMapper.toDTO(cliente));
     }
 }
