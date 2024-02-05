90caa9dc9c3b5c803cf5187af3c71176e7b71cd5
Reis567 <matheusdosreis9@gmail.com>
2024-02-05 16:59:47 -0300

    class name on Repository

--- src/main/java/com/reis/demo/park/api/repository/ClienteRepository.java
+++ src/main/java/com/reis/demo/park/api/repository/ClienteRepository.java
@@ -10,6 +10,6 @@ import com.reis.demo.park.api.repository.projection.ClienteProjection;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
-    @Query("select c from Clientes c")
+    @Query("select c from Cliente c")
     Page<ClienteProjection> findAllPageable(Pageable pageable);
 }
