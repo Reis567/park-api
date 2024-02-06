b14396a87777e333de135f9f7698ecbd7d89b9a0
Reis567 <matheusdosreis9@gmail.com>
2024-02-06 18:54:43 -0300

    method create vaga on service

--- src/main/java/com/reis/demo/park/api/repository/VagaRepository.java
+++ src/main/java/com/reis/demo/park/api/repository/VagaRepository.java
@@ -1,10 +1,15 @@
 package com.reis.demo.park.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.reis.demo.park.api.entity.Vaga;
 
 public interface VagaRepository extends JpaRepository<Vaga, Long>{
+    boolean existsByCodigo(String codigo);
 
+    // Busca uma vaga pelo código
+    Optional<Vaga> findByCodigo(String codigo);
     
 }
--- src/main/java/com/reis/demo/park/api/service/VagaService.java
+++ src/main/java/com/reis/demo/park/api/service/VagaService.java
@@ -1,7 +1,9 @@
 package com.reis.demo.park.api.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.reis.demo.park.api.entity.Vaga;
 import com.reis.demo.park.api.repository.VagaRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -12,4 +14,21 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class VagaService {
     private final VagaRepository vagaRepository;
+
+    @Transactional
+    public Vaga criarVaga(Vaga vaga) throws Exception {
+        try {
+            log.info("Criando vaga: {}", vaga);
+            if (vagaRepository.existsByCodigo(vaga.getCodigo())) {
+                throw new Exception(String.format("Código de vaga '%s' já existe", vaga.getCodigo()));
+            }
+            Vaga createdVaga = vagaRepository.save(vaga);
+            log.info("Vaga criada com sucesso: {}", createdVaga);
+            return createdVaga;
+        } catch (Exception ex) {
+            log.error("Erro ao criar vaga. Motivo: {}", ex.getMessage());
+            throw new Exception(String.format("Erro ao criar vaga. Motivo: '%s'", ex.getMessage()));
+        }
+    }
+    
 }
