79089d7e09d4c1958488e7d0ebd1e7f4f9618b57
Reis567 <matheusdosreis9@gmail.com>
2024-02-06 08:17:05 -0300

    Vaga response DTO

---
+++ src/main/java/com/reis/demo/park/api/web/dto/VagaResponseDTO.java
@@ -0,0 +1,13 @@
+package com.reis.demo.park.api.web.dto;
+
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class VagaResponseDTO {
+
+    private Long id;
+    private String codigo;
+    private String status;
+
+
+}
