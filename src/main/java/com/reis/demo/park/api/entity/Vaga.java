ed159746556a21fcea21b4151688c5809eda0a57
Reis567 <matheusdosreis9@gmail.com>
2024-02-06 07:54:16 -0300

    Entity 'vagas' Columns

--- src/main/java/com/reis/demo/park/api/entity/Vaga.java
+++ src/main/java/com/reis/demo/park/api/entity/Vaga.java
@@ -4,13 +4,27 @@ import java.io.Serializable;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="vagas")
 @EntityListeners(AuditingEntityListener.class)
 public class Vaga implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "codigo", nullable = false, unique = true,length = 4)
+    private String codigo;
+
+    @Column(name="status",nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusVaga status;
+
+    public enum StatusVaga {
+        LIVRE, 
+        OCUPADA
+    }
     
 }
