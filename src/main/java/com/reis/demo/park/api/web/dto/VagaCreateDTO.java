274d8c6d51a281ca20dc99cbee8c74842f58c2f9
Reis567 <matheusdosreis9@gmail.com>
2024-02-06 08:11:05 -0300

    Vaga create DTO

---
+++ src/main/java/com/reis/demo/park/api/web/dto/VagaCreateDTO.java
@@ -0,0 +1,21 @@
+package com.reis.demo.park.api.web.dto;
+
+import com.reis.demo.park.api.entity.Vaga;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class VagaCreateDTO {
+
+    @NotBlank
+    @Size(min = 1, max = 4)
+    private String codigo;
+
+    @NotNull
+    private Vaga.StatusVaga status;
+}
