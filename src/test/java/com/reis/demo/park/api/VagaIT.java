e2480c67823064795336514cdec9e7e54d00e732
Reis567 <matheusdosreis9@gmail.com>
2024-02-07 20:27:43 -0300

    scripts import and test client

--- src/test/java/com/reis/demo/park/api/VagaIT.java
+++ src/test/java/com/reis/demo/park/api/VagaIT.java
@@ -1,4 +1,15 @@
+package com.reis.demo.park.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/vagas/vagas-insert.sql" ,executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/vagas/vagas-delete.sql" ,executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class VagaIT {
-
+        
+    @Autowired
+    WebTestClient testClient;
 }
