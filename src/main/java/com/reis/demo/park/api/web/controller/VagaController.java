1c0071322f1af82bf71644c9f90be96ca1a37f67
Reis567 <matheusdosreis9@gmail.com>
2024-02-07 07:16:06 -0300

    Controller of get by code

--- src/main/java/com/reis/demo/park/api/web/controller/VagaController.java
+++ src/main/java/com/reis/demo/park/api/web/controller/VagaController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +40,12 @@ public class VagaController {
             vagaService.criarVaga(vaga);
             return ResponseEntity.status(HttpStatus.CREATED).body(VagaMapper.toDTO(vaga));
         
+    }
+
+    @GetMapping("/{codigo}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<VagaResponseDTO> getByCodigo(@PathVariable String codigo) {
+        Vaga vaga = vagaService.buscarPorCodigo(codigo);
+        return ResponseEntity.ok(VagaMapper.toDTO(vaga));
     }
 }
