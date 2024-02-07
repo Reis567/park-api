66792a025d357154677ac256d03f8e46da0b8553
Reis567 <matheusdosreis9@gmail.com>
2024-02-07 06:59:07 -0300

    Controller of create vaga

---
+++ src/main/java/com/reis/demo/park/api/web/controller/VagaController.java
@@ -0,0 +1,42 @@
+package com.reis.demo.park.api.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reis.demo.park.api.config.jwt.JwtUserDetails;
+import com.reis.demo.park.api.entity.Vaga;
+import com.reis.demo.park.api.service.VagaService;
+import com.reis.demo.park.api.web.dto.VagaCreateDTO;
+import com.reis.demo.park.api.web.dto.VagaResponseDTO;
+import com.reis.demo.park.api.web.dto.mapper.VagaMapper;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Tag(name = "Vagas", description = "Recurso para criar e buscar vagas de estacionamento.")
+@RestController
+@RequestMapping("/api/v1/vagas")
+@RequiredArgsConstructor
+public class VagaController {
+    @Autowired
+    private VagaService vagaService;
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<VagaResponseDTO> create(@RequestBody @Valid VagaCreateDTO vagaCreateDTO) throws Exception {
+        Vaga vaga = VagaMapper.toVaga(vagaCreateDTO);
+
+            vagaService.criarVaga(vaga);
+            return ResponseEntity.status(HttpStatus.CREATED).body(VagaMapper.toDTO(vaga));
+        
+    }
+}
