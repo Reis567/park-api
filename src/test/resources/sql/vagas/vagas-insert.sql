99fb700bf07331babff3331023ca4b89c5c5bf76
Reis567 <matheusdosreis9@gmail.com>
2024-02-07 20:26:51 -0300

    Scripts of test of vaga

---
+++ src/test/resources/sql/vagas/vagas-insert.sql
@@ -0,0 +1,9 @@
+insert into USUARIOS( id, username,password, role) values (100,'ana@gmail.com','$2a$12$4G1.TGGdDn2BeBBjA/bXZ.xpAIcyjGqupKqYKLG55wxCZsKXgPuIu','ROLE_ADMIN');
+insert into USUARIOS( id, username,password, role) values (101,'JOAO@gmail.com','$2a$12$4G1.TGGdDn2BeBBjA/bXZ.xpAIcyjGqupKqYKLG55wxCZsKXgPuIu','ROLE_CLIENTE');
+insert into USUARIOS( id, username,password, role) values (102,'davi@gmail.com','$2a$12$4G1.TGGdDn2BeBBjA/bXZ.xpAIcyjGqupKqYKLG55wxCZsKXgPuIu','ROLE_CLIENTE');
+
+
+insert into VAGAS(id, codigo,status) VALUES (10,'A-01', 'LIVRE');
+insert into VAGAS(id, codigo,status) VALUES (20,'A-02', 'LIVRE');
+insert into VAGAS(id, codigo,status) VALUES (30,'A-03', 'OCUPADA');
+insert into VAGAS(id, codigo,status) VALUES (40,'A-04', 'LIVRE');
