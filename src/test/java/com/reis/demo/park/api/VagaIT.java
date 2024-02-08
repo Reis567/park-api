68337ca339fc7fa96ce87679cef286a59e322550
Reis567 <matheusdosreis9@gmail.com>
2024-02-08 19:47:53 -0300

    Test : 409 on create

--- src/test/java/com/reis/demo/park/api/VagaIT.java
+++ src/test/java/com/reis/demo/park/api/VagaIT.java
@@ -5,6 +5,7 @@ package com.reis.demo.park.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -12,6 +13,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import com.reis.demo.park.api.entity.Vaga;
 import com.reis.demo.park.api.web.dto.VagaCreateDTO;
 import com.reis.demo.park.api.web.dto.VagaResponseDTO;
+import com.reis.demo.park.api.web.exception.ErrorMessage;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "/sql/vagas/vagas-insert.sql" ,executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
@@ -40,5 +42,21 @@ public class VagaIT {
             org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo("LIVRE");
 
 
+    }
+    @Test
+    public void createVaga_ComCodigoExistente_RetornaErro409() {
+        ErrorMessage errorMessage = testClient
+            .post()
+            .uri("/api/v1/vagas")
+            .headers(JwtAuthentication.getHeaderAuthorization(testClient, "reis@gmail.com", "123456"))
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(new VagaCreateDTO("A-01", Vaga.StatusVaga.LIVRE))
+            .exchange()
+            .expectStatus().isEqualTo(HttpStatus.CONFLICT)
+            .expectBody(ErrorMessage.class)
+            .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(errorMessage).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(errorMessage.getStatus()).isEqualTo(409);
     }
 }
