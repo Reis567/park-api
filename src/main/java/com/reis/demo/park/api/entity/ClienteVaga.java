369d82c16907743c8b43986dc69c64a5d0e96a6a
Reis567 <matheusdosreis9@gmail.com>
2024-02-09 06:24:10 -0300

    Entity and lombok annotations

--- src/main/java/com/reis/demo/park/api/entity/ClienteVaga.java
+++ src/main/java/com/reis/demo/park/api/entity/ClienteVaga.java
@@ -6,9 +6,18 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Table;
+import lombok.*;
 
+@Entity
+@Table(name = "clientes_tem_vagas")
+@Getter@Setter@AllArgsConstructor@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class ClienteVaga {
     @CreatedDate
     @Column(name = "data_criacao")
