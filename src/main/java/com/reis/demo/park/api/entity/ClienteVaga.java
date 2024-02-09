8dcc74d235a9416c93f06533903b81b5a1c2813d
Reis567 <matheusdosreis9@gmail.com>
2024-02-09 06:49:40 -0300

    Hash code|

--- src/main/java/com/reis/demo/park/api/entity/ClienteVaga.java
+++ src/main/java/com/reis/demo/park/api/entity/ClienteVaga.java
@@ -73,4 +73,14 @@ public class ClienteVaga {
     @LastModifiedBy
     @Column(name = "modificado_por")
     private String modificadoPor;
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+
 }
