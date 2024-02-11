c212eb9cd76cc234533d389f099d5baad7dfe59c
Reis567 <matheusdosreis9@gmail.com>
2024-02-11 15:45:59 -0300

    Parking lot initial setup

---
+++ src/main/java/com/reis/demo/park/api/web/dto/EstacionamentoCreateDTO.java
@@ -0,0 +1,8 @@
+package com.reis.demo.park.api.web.dto;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class EstacionamentoCreateDTO {
+    
+}
