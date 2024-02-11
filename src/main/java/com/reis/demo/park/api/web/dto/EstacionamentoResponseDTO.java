d860f2a586cc20e461e336478e5321e3df89bf49
Reis567 <matheusdosreis9@gmail.com>
2024-02-11 16:15:15 -0300

    Parkinglot Response DTO

---
+++ src/main/java/com/reis/demo/park/api/web/dto/EstacionamentoResponseDTO.java
@@ -0,0 +1,12 @@
+package com.reis.demo.park.api.web.dto;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class EstacionamentoResponseDTO {
+    
+}
