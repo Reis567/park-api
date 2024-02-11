0cc799337432597c558a49fd0be7c50ec1f87e45
Reis567 <matheusdosreis9@gmail.com>
2024-02-11 16:18:06 -0300

    Parkinglot Response DTO fields

--- src/main/java/com/reis/demo/park/api/web/dto/EstacionamentoResponseDTO.java
+++ src/main/java/com/reis/demo/park/api/web/dto/EstacionamentoResponseDTO.java
@@ -1,4 +1,9 @@
 package com.reis.demo.park.api.web.dto;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.validator.constraints.br.CPF;
+
 import jakarta.validation.constraints.*;
 import lombok.*;
 
@@ -9,4 +14,27 @@ import lombok.*;
 @ToString
 public class EstacionamentoResponseDTO {
     
+    private String placa;
+
+    private String marca;
+
+
+    private String modelo;
+
+
+    private String cor;
+
+    private String clienteCpf;
+
+    private String recibo;
+
+    private LocalDateTime dataEntrada;
+    
+    private LocalDateTime dataSaida;
+
+    private String vagaCodigo;
+
+    private BigDecimal valor;
+
+    private BigDecimal desconto;
 }
