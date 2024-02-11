15f5a87f4ba8f2845dff1d8b0dabeb11351a7ad4
Reis567 <matheusdosreis9@gmail.com>
2024-02-11 16:03:59 -0300

    Parkinglot Create DTO

--- src/main/java/com/reis/demo/park/api/web/dto/EstacionamentoCreateDTO.java
+++ src/main/java/com/reis/demo/park/api/web/dto/EstacionamentoCreateDTO.java
@@ -1,8 +1,32 @@
 package com.reis.demo.park.api.web.dto;
+
+import org.hibernate.validator.constraints.br.CPF;
+
 import jakarta.validation.constraints.*;
 import lombok.*;
 
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class EstacionamentoCreateDTO {
-    
+
+    @NotBlank
+    @Pattern(regexp = "^[A-Z]{3}[0-9][A-Z0-9][0-9]{2}$", message = "A placa deve conter apenas letras, números e ter 7 caracteres.")
+    private String placa;
+
+    @NotBlank
+    private String marca;
+
+    @NotBlank
+    private String modelo;
+
+    @NotBlank
+    private String cor;
+
+    @NotBlank
+    @Size(min = 11, max = 11)
+    @CPF
+    private String clienteCpf;
 }
