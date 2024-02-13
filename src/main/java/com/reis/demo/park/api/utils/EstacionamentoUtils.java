85ae19d2c22a5a2358c8cab4392d2321494cdb41
Reis567 <matheusdosreis9@gmail.com>
2024-02-13 16:52:09 -0300

    ParkingLot utils

---
+++ src/main/java/com/reis/demo/park/api/utils/EstacionamentoUtils.java
@@ -0,0 +1,11 @@
+package com.reis.demo.park.api.utils;
+
+import java.time.LocalDateTime;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EstacionamentoUtils {
+
+}
