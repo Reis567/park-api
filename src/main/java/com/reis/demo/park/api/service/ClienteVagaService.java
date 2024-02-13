422d24ba173b9232a65d7032154eb9a84905418d
Reis567 <matheusdosreis9@gmail.com>
2024-02-13 17:11:55 -0300

    initial setup to add method save

--- src/main/java/com/reis/demo/park/api/service/ClienteVagaService.java
+++ src/main/java/com/reis/demo/park/api/service/ClienteVagaService.java
@@ -6,10 +6,16 @@ import com.reis.demo.park.api.repository.ClienteVagaRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ClienteVagaService {
     private final ClienteVagaRepository clienteVagaRepository;
+
+    @Transactional
+    public ClienteVaga salvar(){
+        
+    }
 }
