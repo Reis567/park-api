02d4798af9d52597a810095788238d62c940261e
Reis567 <matheusdosreis9@gmail.com>
2024-02-13 17:13:16 -0300

    add method save

--- src/main/java/com/reis/demo/park/api/service/ClienteVagaService.java
+++ src/main/java/com/reis/demo/park/api/service/ClienteVagaService.java
@@ -2,7 +2,9 @@ package com.reis.demo.park.api.service;
 
 import org.springframework.stereotype.Service;
 
+import com.reis.demo.park.api.entity.ClienteVaga;
 import com.reis.demo.park.api.repository.ClienteVagaRepository;
+import com.reis.demo.park.api.utils.EstacionamentoUtils;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +17,15 @@ public class ClienteVagaService {
     private final ClienteVagaRepository clienteVagaRepository;
 
     @Transactional
-    public ClienteVaga salvar(){
-        
+    public ClienteVaga salvar(ClienteVaga clienteVaga) {
+        try {
+            //clienteVaga.setRecibo(EstacionamentoUtils.gerarRecibo());
+
+            log.info("Salvando clienteVaga: {}", clienteVaga);
+            return clienteVagaRepository.save(clienteVaga);
+        } catch (Exception ex) {
+            log.error("Erro ao salvar clienteVaga. Motivo: {}", ex.getMessage());
+            throw ex;
+        }
     }
 }
