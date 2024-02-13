f45181d39674666ed53c3893e5e08a5f08eaf7e0
Reis567 <matheusdosreis9@gmail.com>
2024-02-13 09:19:22 -0300

    To client parking lot method

--- src/main/java/com/reis/demo/park/api/web/dto/mapper/ClienteVagaMapper.java
+++ src/main/java/com/reis/demo/park/api/web/dto/mapper/ClienteVagaMapper.java
@@ -1,9 +1,16 @@
 package com.reis.demo.park.api.web.dto.mapper;
 
+import org.modelmapper.ModelMapper;
+
+import com.reis.demo.park.api.entity.ClienteVaga;
+import com.reis.demo.park.api.web.dto.EstacionamentoCreateDTO;
+
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ClienteVagaMapper {
-    
+    public static ClienteVaga toClienteVaga(EstacionamentoCreateDTO estacionamentoCreateDTO){
+        return new ModelMapper().map(estacionamentoCreateDTO, ClienteVaga.class);
+    }
 }
