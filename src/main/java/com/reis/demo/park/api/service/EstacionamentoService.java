1260c84b8df9e0a5c8f0e851c082fcfbfcac84fe
Reis567 <matheusdosreis9@gmail.com>
2024-02-14 07:49:06 -0300

    add initial checkIn logic , getting the client by the cpf

--- src/main/java/com/reis/demo/park/api/service/EstacionamentoService.java
+++ src/main/java/com/reis/demo/park/api/service/EstacionamentoService.java
@@ -2,6 +2,9 @@ package com.reis.demo.park.api.service;
 
 import org.springframework.stereotype.Service;
 
+import com.reis.demo.park.api.entity.Cliente;
+import com.reis.demo.park.api.entity.ClienteVaga;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,5 +18,9 @@ public class EstacionamentoService {
     private final ClienteService clienteService;
 
     private final VagaService vagaService;
+
+    public ClienteVaga checkIn(ClienteVaga clienteVaga){
+        Cliente cliente = clienteService.buscarPorCpf(clienteVaga.getCliente().getCpf());
+    }
     
 }
