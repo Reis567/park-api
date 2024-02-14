c37b3cea63d8f79b008384a58afed96f86be0d42
Reis567 <matheusdosreis9@gmail.com>
2024-02-14 08:45:18 -0300

    Set the date time , and the recibo

--- src/main/java/com/reis/demo/park/api/service/EstacionamentoService.java
+++ src/main/java/com/reis/demo/park/api/service/EstacionamentoService.java
@@ -1,11 +1,14 @@
 package com.reis.demo.park.api.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.reis.demo.park.api.entity.Cliente;
 import com.reis.demo.park.api.entity.ClienteVaga;
 import com.reis.demo.park.api.entity.Vaga;
+import com.reis.demo.park.api.utils.EstacionamentoUtils;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,6 +31,9 @@ public class EstacionamentoService {
         Vaga vaga = vagaService.buscarPorVagaLivre();
         vaga.setStatus(Vaga.StatusVaga.OCUPADA);
         clienteVaga.setVaga(vaga);
+
+        clienteVaga.setDataEntrada(LocalDateTime.now());
+        clienteVaga.setRebibo(EstacionamentoUtils.gerarRecibo());
     }
     
 }
