fb8bf5cda1a14355936576ab288f80a6c9eb0ddb
Reis567 <matheusdosreis9@gmail.com>
2024-02-15 18:41:03 -0300

    Convert the DTO to Cliente vaga

--- src/main/java/com/reis/demo/park/api/web/controller/EstacionamentoController.java
+++ src/main/java/com/reis/demo/park/api/web/controller/EstacionamentoController.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.reis.demo.park.api.entity.ClienteVaga;
 import com.reis.demo.park.api.service.EstacionamentoService;
+import com.reis.demo.park.api.web.dto.EstacionamentoCreateDTO;
 import com.reis.demo.park.api.web.dto.EstacionamentoResponseDTO;
+import com.reis.demo.park.api.web.dto.mapper.ClienteVagaMapper;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -23,7 +26,9 @@ public class EstacionamentoController {
 
     @PostMapping("/check-in")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<EstacionamentoResponseDTO> checkin(@RequestBody @Valid EstacionamentoResponseDTO estacionamentoResponseDTO){
+    public ResponseEntity<EstacionamentoResponseDTO> checkin(@RequestBody @Valid EstacionamentoCreateDTO estacionamentoCreateDTO){
+            ClienteVaga clienteVaga = ClienteVagaMapper.toClienteVaga(estacionamentoCreateDTO);
+
 
     }
 }
