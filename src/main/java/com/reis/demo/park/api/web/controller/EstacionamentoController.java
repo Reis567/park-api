9c822fbaf34769c9175d0909fd0d4ef409345548
Reis567 <matheusdosreis9@gmail.com>
2024-02-15 18:14:43 -0300

    Controller : checkin endpoint , only admin can call

--- src/main/java/com/reis/demo/park/api/web/controller/EstacionamentoController.java
+++ src/main/java/com/reis/demo/park/api/web/controller/EstacionamentoController.java
@@ -1,9 +1,17 @@
 package com.reis.demo.park.api.web.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.reis.demo.park.api.service.EstacionamentoService;
+import com.reis.demo.park.api.web.dto.EstacionamentoResponseDTO;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Tag(name="Estacionamentos", description = "")
@@ -11,5 +19,11 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("api/v1/estacionamentos")
 @RequiredArgsConstructor
 public class EstacionamentoController {
-    
+    private final EstacionamentoService estacionamentoService;
+
+    @PostMapping("/check-in")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<EstacionamentoResponseDTO> checkin(@RequestBody @Valid EstacionamentoResponseDTO estacionamentoResponseDTO){
+
+    }
 }
