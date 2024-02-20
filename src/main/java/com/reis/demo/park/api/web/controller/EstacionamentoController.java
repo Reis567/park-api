1845e93bbc136dd68a0a81cf6c924f270802a55a
Reis567 <matheusdosreis9@gmail.com>
2024-02-20 19:11:00 -0300

    feat : initial setup of controller to get by recibo

--- src/main/java/com/reis/demo/park/api/web/controller/EstacionamentoController.java
+++ src/main/java/com/reis/demo/park/api/web/controller/EstacionamentoController.java
@@ -5,10 +5,7 @@ import java.net.URI;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.reis.demo.park.api.entity.ClienteVaga;
@@ -69,5 +66,10 @@ public class EstacionamentoController {
                             .toUri();
 
             return ResponseEntity.status(HttpStatus.CREATED).location(location).body(responseDTO);
+    }
+    
+    @GetMapping("/check-in/{recibo}")
+    public ResponseEntity<EstacionamentoResponseDTO> getByRecibo(@PathVariable String recibo){
+
     }
 }
