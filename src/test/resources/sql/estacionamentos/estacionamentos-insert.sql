9a5ca9a11cd471f8c6cab13c599c13b63928c497
Reis567 <matheusdosreis9@gmail.com>
2024-02-23 08:07:37 -0300

    New cliente and user on tests script

--- src/test/resources/sql/estacionamentos/estacionamentos-insert.sql
+++ src/test/resources/sql/estacionamentos/estacionamentos-insert.sql
@@ -2,10 +2,12 @@ insert into USUARIOS( id, username,password, role) values (100,'ana@gmail.com','
 insert into USUARIOS( id, username,password, role) values (101,'JOAO@gmail.com','$2a$12$4G1.TGGdDn2BeBBjA/bXZ.xpAIcyjGqupKqYKLG55wxCZsKXgPuIu','ROLE_CLIENTE');
 insert into USUARIOS( id, username,password, role) values (102,'davi@gmail.com','$2a$12$4G1.TGGdDn2BeBBjA/bXZ.xpAIcyjGqupKqYKLG55wxCZsKXgPuIu','ROLE_CLIENTE');
 insert into USUARIOS( id, username,password, role) values (103,'reis@gmail.com','$2a$12$4G1.TGGdDn2BeBBjA/bXZ.xpAIcyjGqupKqYKLG55wxCZsKXgPuIu','ROLE_ADMIN');
+insert into USUARIOS( id, username,password, role) values (104,'cliente@gmail.com','$2a$12$4G1.TGGdDn2BeBBjA/bXZ.xpAIcyjGqupKqYKLG55wxCZsKXgPuIu','ROLE_CLIENTE');
 
 insert into CLIENTES(id,nome, cpf, id_usuario) values(20, 'Victor sá', '23794879520', 100);
 insert into CLIENTES(id,nome, cpf, id_usuario) values(21, 'Fernandinho Beiramar', '59575966392', 101);
 insert into CLIENTES(id,nome, cpf, id_usuario) values(22, 'Alexander Barboza', '17526942360', 102);
+insert into CLIENTES(id,nome, cpf, id_usuario) values(23, 'Mateo ponte', '17526942360', 104);
 
 
 insert into VAGAS(id, codigo,status) VALUES (100,'A-01', 'OCUPADA');
